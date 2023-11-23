1793b051dd496549a639d27773db11d996428b21
Otavio Menezes <romenezes@stefanini.com>
2023-11-22 23:43:00 -0300

    create user type enum

---
+++ src/main/java/br/com/apisimplepicpay/infra/adapters/enums/UserTypeEnum.java
@@ -0,0 +1,35 @@
+package br.com.apisimplepicpay.infra.adapters.enums;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+public enum UserTypeEnum {
+    COMMON("common"),
+    MERCHANT("merchant");
+
+    private String type;
+
+    UserTypeEnum(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Converter(autoApply = true)
+    public static class Mapper implements AttributeConverter<UserTypeEnum, String> {
+        @Override
+        public String convertToDatabaseColumn(UserTypeEnum attribute) {
+            return String.valueOf(attribute.getType());
+        }
+
+        @Override
+        public UserTypeEnum convertToEntityAttribute(String dbData) {
+            if (dbData == null) return null;
+            else if ("common".equalsIgnoreCase(dbData)) return COMMON;
+            else if ("merchant".equalsIgnoreCase(dbData)) return MERCHANT;
+            throw new IllegalStateException();
+        }
+    }
+}
